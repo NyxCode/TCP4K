7bca02609c577bfef40bc69a2b1176f9e64baf44
NyxCode <NyxCode>
2017-06-18 13:04:31 +0200

    Add superclass cache in DefaultListenerHandler.kt

--- tcp4k/src/main/kotlin/de/nyxcode/tcp4k/impl/DefaultListenerHandler.kt
+++ tcp4k/src/main/kotlin/de/nyxcode/tcp4k/impl/DefaultListenerHandler.kt
@@ -3,12 +3,14 @@ package de.nyxcode.tcp4k.impl
 import de.nyxcode.tcp4k.Connection
 import de.nyxcode.tcp4k.Listener
 import de.nyxcode.tcp4k.ListenerHandler
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.CopyOnWriteArrayList
 import kotlin.reflect.KClass
 import kotlin.reflect.full.superclasses
 
 class DefaultListenerHandler: ListenerHandler {
-    private val listener = HashMap<KClass<*>, MutableCollection<Listener<*>>>()
+    private val listener = ConcurrentHashMap<KClass<*>, MutableCollection<Listener<*>>>()
+    private val superClasses = ConcurrentHashMap<KClass<*>, Collection<KClass<*>>>()
 
     override fun <T : Any> register(clazz: KClass<T>, listener: Listener<T>) {
         this.listener.computeIfAbsent(clazz) { CopyOnWriteArrayList<Listener<*>>() }.add(listener)
@@ -18,10 +20,19 @@ class DefaultListenerHandler: ListenerHandler {
         val type = obj::class
         listener[Any::class]?.triggerAll(con, obj)
         listener[type]?.triggerAll(con, obj)
-        type.superclasses.forEach { listener[it]?.triggerAll(con, obj) }
+        cachedSuperClasses(type).forEach { listener[it]?.triggerAll(con, obj) }
     }
 
     @Suppress("UNCHECKED_CAST")
     private fun <T : Any> Collection<Listener<*>>.triggerAll(con: Connection, msg: T) =
             forEach { (it as Listener<T>).received(con, msg) }
+
+    private fun <T : Any> cachedSuperClasses(clazz: KClass<T>): Collection<KClass<*>> {
+        var superclasses = superClasses[clazz]
+        if(superclasses == null) {
+            superclasses = clazz.superclasses
+            superClasses[clazz] = superclasses
+        }
+        return superclasses
+    }
 }
